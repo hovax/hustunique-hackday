886b1ccfa13e24bd5ed7da5bd789355fcc99a6a8
Xinran Li <uxizde@gmail.com>
2014-05-25 02:27:21 +0000

    fix config

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 import os
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vupceygjtznrut:rJ7SkS5uslvwmHJ6dP5qKsWhwR@ec2-50-17-207-54.compute-1.amazonaws.com:5432/dfo5fmvrg65dhg'
 DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'pass'
 HOST = '0.0.0.0'
